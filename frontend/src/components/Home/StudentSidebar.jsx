89dff2086bd939c9a628d5e47de5e2930ef47269
Nikunj Sharma <nikunjsharma17rtm@gmail.com>
2025-01-12 14:33:10 +0530

    update: modify the student sidebar

--- frontend/src/components/Home/StudentSidebar.jsx
+++ frontend/src/components/Home/StudentSidebar.jsx
@@ -1,29 +1,33 @@
 import {
     HomeIcon,
-    ClipboardListIcon,
-    FolderIcon,
     ChartSquareBarIcon,
     SparklesIcon,
     BellIcon,
     CogIcon,
     LogoutIcon,
     UserGroupIcon,
+    UserCircleIcon,
+    StarIcon,
+    ClipboardCheckIcon,
+    ViewListIcon,
+    ArchiveIcon
 } from "@heroicons/react/outline";
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/AuthContext";
-import { useNavigate, useLocation } from "react-router-dom"; // Import useLocation
+import { useNavigate, useLocation } from "react-router-dom"; 
 import { jwtDecode } from "jwt-decode";
+import Tooltip from './Tooltip_sidebar'; 
 
 const Sidebar = ({ onToggle }) => {
     const [isCollapsed, setIsCollapsed] = useState(false);
     const { isAuthenticated, setIsAuthenticated } = useAuth();
     const navigate = useNavigate();
-    const location = useLocation(); // Get the current location
+    const location = useLocation(); 
     const email = localStorage.getItem("emailCont");
 
     const toggleSidebar = () => {
         setIsCollapsed(!isCollapsed);
-        onToggle(!isCollapsed); // Notify the parent about the change
+        onToggle(!isCollapsed); 
     };
 
     const handleLogout = () => {
@@ -38,7 +42,7 @@ const Sidebar = ({ onToggle }) => {
             if (token) {
                 try {
                     const decoded = jwtDecode(token);
-                    const currentTime = Date.now() / 1000; // Current time in seconds
+                    const currentTime = Date.now() / 1000; 
                     if (decoded.exp > currentTime) {
                         setIsAuthenticated(true);
                     } else {
@@ -56,25 +60,42 @@ const Sidebar = ({ onToggle }) => {
 
         checkAuthStatus();
 
-        const interval = setInterval(checkAuthStatus, 10000); // Check every 10 seconds
+        const interval = setInterval(checkAuthStatus, 10000); 
         return () => clearInterval(interval);
     }, []);
 
     const menuItems = [
         { icon: <HomeIcon className="h-6 w-6" />, label: "Home", path: "/home" },
-        { icon: <UserGroupIcon GroupIcon className="h-6 w-6" />, label: "All Clubs", path: "/Clubs" },
-        { icon: <ClipboardListIcon className="h-6 w-6" />, label: "My Events", path: "/my-events" },
-        { icon: <ClipboardListIcon className="h-6 w-6" />, label: "All Events", path: "/all-events" },
+        { icon: <UserGroupIcon className="h-6 w-6" />, label: "All Clubs", path: "/clubs" },
+        { icon: <ClipboardCheckIcon className="h-6 w-6" />, label: "My Events", path: "/my-events" },
+        { icon: <ArchiveIcon className="h-6 w-6" />, label: "All Events", path: "/all-events" },
         { icon: <SparklesIcon className="h-6 w-6" />, label: "My Batch Leaderboard", path: "/batch-leaderboard" },
-        { icon: <SparklesIcon className="h-6 w-6" />, label: "Overall Leaderboard", path: "/overall-leaderboard" },
+        { icon: <StarIcon className="h-6 w-6" />, label: "Overall Leaderboard", path: "/overall-leaderboard" },
         { icon: <ChartSquareBarIcon className="h-6 w-6" />, label: "My Achievements", path: "/my-achievements" },
-        { icon: <UserGroupIcon GroupIcon className="h-6 w-6" />, label: "My Profile", path: "/profile" },
+        { icon: <UserCircleIcon className="h-6 w-6" />, label: "My Profile", path: "/profile" },
     ];
+
+    // Handle window resize to collapse sidebar on small screens
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 820) { 
+                setIsCollapsed(true);
+            } else {
+                setIsCollapsed(false);
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        handleResize(); // Call on mount to set initial state
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
 
     return (
         <div
-            className={`fixed top-0 left-0 h-screen bg-gray-800 text-white ${isCollapsed ? "w-16" : "w-64"
-                } transition-all duration-300 z-50`}
+            className={`fixed top-0 left-0 h-screen dark:bg-gray-800 dark:text-white bg-gray-200 text-gray-800 ${isCollapsed ? "w-16" : "w-64"} transition-all duration-300 z-50`}
         >
             {/* Sidebar Header */}
             <div className="flex items-center justify-between p-4">
@@ -84,7 +105,7 @@ const Sidebar = ({ onToggle }) => {
                     )}
                 </div>
                 <button
-                    className="p-2 rounded hover:bg-gray-700"
+                    className="p-2 rounded hover:bg-gray-300 dark:hover:bg-gray-700"
                     onClick={toggleSidebar}
                 >
                     <span className="material-icons">
@@ -94,14 +115,14 @@ const Sidebar = ({ onToggle }) => {
             </div>
 
             {/* Sidebar Menu */}
-            <ul className="mt-6 space-y-2">
+            <ul className="mt-6 ml-3 space-y-2">
                 {menuItems.map((item, idx) => (
                     <li
                         key={idx}
                         className={`flex items-center space-x-4 p-2 rounded cursor-pointer
                             ${location.pathname === item.path
-                                ? "bg-blue-600 text -white shadow-md" // Active style
-                                : "hover:bg-gray-700"
+                                ? "bg-blue-600 text-white shadow-md" // Active style
+                                : "hover:bg-gray-300 dark:hover:bg-gray-700"
                             }`}
                         onClick={() => {
                             console.log("isAuthenticated ", isAuthenticated);
@@ -111,14 +132,16 @@ const Sidebar = ({ onToggle }) => {
                             navigate(item.path);
                         }}
                     >
-                        <span className="text-lg">{item.icon}</span>
+                        <Tooltip text={item.label} show={isCollapsed}>
+                            <span className="text-lg">{item.icon}</span>
+                        </Tooltip>
                         {!isCollapsed && <span>{item.label}</span>}
                     </li>
                 ))}
             </ul>
 
             {/* Sidebar Footer */}
-            <div className="absolute bottom-4">
+            <div className="absolute bottom-4 ml-3">
                 <ul className="space-y-2">
                     {[{
                         icon: <BellIcon className="h-6 w-6" />,
@@ -135,16 +158,18 @@ const Sidebar = ({ onToggle }) => {
                             className={`flex items-center space-x-4 p-2 rounded cursor-pointer
                                 ${location.pathname === item.path
                                     ? "bg-blue-600 text-white shadow-md" // Active style
-                                    : "hover:bg-gray-700"
+                                    : "hover:bg-gray-300 dark:hover:bg-gray-700"
                                 }`}
                             onClick={() => navigate(item.path)}
                         >
-                            <span className="text-lg">{item.icon}</span>
+                            <Tooltip text={item.label} show={isCollapsed}>
+                                <span className="text-lg">{item.icon}</span>
+                            </Tooltip>
                             {!isCollapsed && <span>{item.label}</span>}
                         </li>
                     ))}
                 </ul>
-                <div className="flex items-center mt-4 p-2 hover:bg-gray-700 rounded">
+                <div className="flex items-center mt-4 p-2 hover:bg-gray-300 dark:hover:bg-gray-700 rounded">
                     <div className="h-8 w-8 bg-gray-500 rounded-full"></div>
                     {!isCollapsed && (
                         <div className="ml-2">
@@ -157,11 +182,11 @@ const Sidebar = ({ onToggle }) => {
                 </div>
                 {/* Log Out Button */}
                 <div
-                    className="flex items-center mt-4 p-2 hover:bg-gray-700 rounded cursor-pointer"
+                    className="flex items-center mt-4 p-2 hover:bg-gray-300 dark:hover:bg-gray-700 rounded cursor-pointer"
                     onClick={handleLogout}
                 >
                     <LogoutIcon className="h-6 w-6" />
-                    {!isCollapsed && <span className="ml-4">Log Out</span>}
+                    {!isCollapsed && <span className="ml-5">Log Out</span>}
                 </div>
             </div>
         </div>
